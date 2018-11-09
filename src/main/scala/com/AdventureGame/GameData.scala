3f163e61b7cc53ed7f0ee62cff0a4b78ad2a6cdc
julian.saunders <julian.saunders@digital.hmrc.gov.uk>
2018-11-09 17:25:14 +0000

    add objects

--- src/main/scala/com/AdventureGame/GameData.scala
+++ src/main/scala/com/AdventureGame/GameData.scala
@@ -3,7 +3,7 @@ package com.AdventureGame
 import scala.collection.mutable
 import scala.collection.mutable.{HashMap, MultiMap, Set}
 
-sealed case class StepChoices(optionVal: String, description: String, redirectStep: String)
+final case class StepChoices(optionVal: String, description: String, redirectStep: String)
 
 sealed trait stepDataT {
   val name :String
@@ -12,23 +12,27 @@ sealed trait stepDataT {
   val description: String
 }
 
-sealed trait choicesDataT{
-  val choices : Seq[StepChoices]
-}
-
 case class StepData(name :String,
                     dieOnThisStep: Boolean,
                     winOnThisStep: Boolean,
-                    description: String
-                    ) extends stepDataT{
+                    description: String) extends stepDataT{
 }
-
-case class ChoicesData(choices : Seq[StepChoices]
-                      )extends choicesDataT {}
 
 trait OrderedMultimap[A, B] extends MultiMap[A, B] {
   override def makeSet: Set[B] = new mutable.LinkedHashSet[B]
 }
+
+
+sealed trait gameObjectT{
+  val optionVal: String
+  val description : String
+}
+
+case class Weapon(optionVal: String, description : String)extends gameObjectT {}
+case class OtherObject(optionVal: String, description : String)extends gameObjectT {}
+case class Person(optionVal: String, description : String)extends gameObjectT {}
+case class Animal(optionVal: String, description : String)extends gameObjectT {}
+
 
 object AllGameData {
 
@@ -37,6 +41,8 @@ object AllGameData {
   var allLocation = scala.collection.immutable.Map[String, StepData]()
   // https://alvinalexander.com/java/jwarehouse/scala-2.11/library/scala/collection/mutable/MultiMap.scala.shtml
   val allChoice = new HashMap[String, Set[StepChoices]] with OrderedMultimap[String, StepChoices]
+  val allLocationObject = new HashMap[String, Set[gameObjectT]] with OrderedMultimap[String, gameObjectT]
+
 
   allLocation += ("Buckingham-Palace-gate" -> StepData("buckingham-palace", false, false, "You are standing at the front of Buckingham Palace gates"))
   allChoice.addBinding("Buckingham-Palace-gate", StepChoices("1", "Walk up Constitution Hill", "Constitution-Hill"))
@@ -44,6 +50,9 @@ object AllGameData {
   allChoice.addBinding("Buckingham-Palace-gate", StepChoices("3", "Walk to the Queen Victoria Statue", "Queen-Victoria-Statue"))
   allChoice.addBinding("Buckingham-Palace-gate", StepChoices("4", "Enter Green Park", "Green-Park-buckingham-palace"))
   allChoice.addBinding("Buckingham-Palace-gate", StepChoices("5", "Walk down Spur Road", "Spur-Road"))
+  allLocationObject.addBinding("Buckingham-Palace-gate", OtherObject("1", "Crisp packet"))
+  allLocationObject.addBinding("Buckingham-Palace-gate", Weapon("1", "Tree branch"))
+
 
   allLocation += ("Constitution-Hill" -> StepData("Constitution-Hill", false, false, "You are walking up Constitution Hill"))
   allChoice.addBinding("Constitution-Hill", StepChoices("1", "Walk to Hyde Park Corner", "Hyde-Park-Corner"))
@@ -60,6 +69,10 @@ object AllGameData {
   allLocation += ("Marble-Arch" -> StepData("Marble-Arch", false, false, "You are standing at Marble Arch"))
   allChoice.addBinding("Marble-Arch", StepChoices("1", "Walk through the park towards Hyde Park Corner", "Hyde-Park-Bottom-East"))
   allChoice.addBinding("Marble-Arch", StepChoices("2", "Walk along Oxford Street", "Oxford-Street-West"))
+  allLocationObject.addBinding("Marble-Arch", Animal("1", "Horse"))
+  allLocationObject.addBinding("Marble-Arch", Person("2", "Police person on horseback"))
+
+
 
 
 }
--- src/main/scala/com/AdventureGame/LocationService.scala
+++ src/main/scala/com/AdventureGame/LocationService.scala
@@ -20,31 +20,98 @@ object LocationService {
       printLocationAndGetChoice(lastLocationId, lastLocationId, subtractOneStep(playerData))
     }
     else {
-
       val location: StepData = allLocation(locationId)
-      fprint("")
-      fprint(location.description)
-      fprint("Steps taken : " + playerData.steps)
-      fprint("You have the following options :")
+      val choices: Option[Set[StepChoices]] = allChoice.get(locationId)
+      val objects: Option[Set[gameObjectT]] = allLocationObject.get(locationId)
 
-      val choices: Set[StepChoices] = allChoice(locationId)
-      choices.map(c => fprint(c.optionVal + " : " + c.description, choices = true))
 
-      fprint("")
-      val playerChoice = readLine("*   You >>> ")
+      printLocationDescription(location)
+      printStepCount(playerData)
+      printChoices(choices)
+      printObjects(objects)
+      val playerChoice = getUserChoiceInput()
+
       playerChoice match {
         case `exitGameCommand` => fprint("Goodbye " + playerData.name + ", thank you for playing")
         case _ => {
-          val selectedChoice = choices.find(s => s.optionVal == playerChoice.trim.toUpperCase())
-          selectedChoice match {
-            case None => {
-              fprint("You entered an incorrect value, please try again.\n\n");
-              printLocationAndGetChoice(locationId, locationId, playerData)
+          choices match {
+            case None =>
+            case Some(c) => {
+              val selectedChoice = c.find(s => s.optionVal == playerChoice.trim.toUpperCase())
+              selectedChoice match {
+                case None => {
+                  fprint("You entered an incorrect value, please try again.\n\n");
+                  printLocationAndGetChoice(locationId, locationId, playerData)
+                }
+                case Some(StepChoices(_, _, newLocationId)) => printLocationAndGetChoice(newLocationId, locationId, addOneStep(playerData))
+              }
             }
-            case Some(StepChoices(_, _, newLocationId)) => printLocationAndGetChoice(newLocationId, locationId, addOneStep(playerData))
           }
         }
       }
     }
+  }
+
+  def printLocationDescription(location: StepData): Unit = {
+    fprint("")
+    fprint(location.description)
+  }
+
+  def printStepCount(playerData: PlayerData): Unit = {
+    fprint("Steps taken : " + playerData.steps)
+  }
+
+  def printChoices(choices: Option[Set[StepChoices]]): Unit = {
+    fprint("You have the following options :")
+    choices match {
+      case None =>
+      case Some(c) => c.map(c => fprint(c.optionVal + " : " + c.description, choices = true))
+    }
+  }
+
+  def printObjects(objs: Option[Set[gameObjectT]]): Unit = {
+    objs match {
+      case None =>
+      case Some(objects) => {
+        if (objects.exists(o => o match {
+          case _: Weapon | _: OtherObject => true
+          case _ => false
+        })) {
+          fprint("There are the following objects that can be picked up :")
+          objects.map {
+            case o: Weapon => fprint("W" + o.optionVal + " : " + o.description, choices = true)
+            case o: OtherObject => fprint("O" + o.optionVal + " : " + o.description, choices = true)
+            case o@(_: Person | _: Animal) => //I left this in here as an example of how to handle 2 types with 1 command, instead of case _ =>
+          }
+        }
+
+        if (objects.exists(o => o match {
+          case _: Person => true
+          case _ => false
+        })) {
+          fprint("There are the following persons :")
+          objects.map {
+            case o: Person => fprint("P" + o.optionVal + " : " + o.description, choices = true)
+            case _ =>
+          }
+        }
+
+        if (objects.exists(o => o match {
+          case _: Person => true
+          case _ => false
+        })) {
+          fprint("There are the following animals :")
+          objects.map {
+            case o: Animal => fprint("A" + o.optionVal + " : " + o.description, choices = true)
+            case _ =>
+          }
+        }
+      }
+    }
+  }
+
+  def getUserChoiceInput() :String ={
+    fprint("")
+    readLine("*   You >>> ")
   }
 }
